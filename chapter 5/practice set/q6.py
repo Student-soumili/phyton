fafc2cb55d1b450c284ae9f6d0a61939079ca394
soumili duttta <soumilid05@gmail.com>
2025-08-24 02:07:31 +0530

    initial comment

---
+++ chapter 5/practice set/q6.py
@@ -0,0 +1,32 @@
+d = {}
+
+name = input("Enter friend name:")
+lang = input("Enter language name:")
+d.update({name : lang})
+
+name = input("Enter friend name:")
+lang = input("Enter language name:")
+d.update({name : lang})
+name = input("Enter friend name:")
+lang = input("Enter language name:")
+d.update({name : lang})
+
+name = input("Enter friend name:")
+lang = input("Enter language name:")
+d.update({name : lang})
+
+name = input("Enter friend name:")
+lang = input("Enter language name:")
+d.update({name : lang})
+
+name = input("Enter friend name:")
+lang = input("Enter language name:")
+d.update({name : lang})
+
+d.update({name : lang})
+
+name = ("Enter friend name:")
+lang =("Enter language name:")
+d.update({name : lang})
+
+print(d)
