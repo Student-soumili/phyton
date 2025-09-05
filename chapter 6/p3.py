2c63b47fd5e4d37dc65f3eb373bc69011ca454eb
soumili duttta <soumilid05@gmail.com>
2025-09-05 17:21:14 +0530

    initial comment

---
+++ chapter 6/p3.py
@@ -0,0 +1,8 @@
+# Program to check if a year is a leap year
+
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(f"{year} is a Leap Year")
+else:
+    print(f"{year} is NOT a Leap Year")
