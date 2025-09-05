e532e27910f43fa587c7fba42099ad9602dc0615
soumili duttta <soumilid05@gmail.com>
2025-09-05 17:14:27 +0530

    initial comment

---
+++ chapter 6/p2.py
@@ -0,0 +1,10 @@
+# Check if a number is positive, negative, or zero
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print(f"{num} is Positive")
+elif num < 0:
+    print(f"{num} is Negative")
+else:
+    print("The number is Zero")
